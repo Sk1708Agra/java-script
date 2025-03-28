62b634720c41d0ac4e230a2547e160104c50bbf9
Shubham Kulshreshtha <66267171+Sk1708Agra@users.noreply.github.com>
2025-03-28 09:01:19 +0530

    Update test.js

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,12 @@
 console.log("Shubham")
+
+console.log("Shubham")
+const employId = 2315990067
+let employCity = "Agra"
+let employEmail = "kulshreshthask1708@gmail.com"
+let employPass = "123456789"
+
+employPass = "2315990055"
+employCity = "Aligarh"
+
+console.table([employId, employCity, employEmail, employPass])
