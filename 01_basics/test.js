f25d493c057628553e10b3a7af0f62a3b65fc92b
Shubham Kulshreshtha <66267171+Sk1708Agra@users.noreply.github.com>
2025-03-28 09:13:31 +0530

    Update test.js

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -7,6 +7,7 @@ let employEmail = "kulshreshthask1708@gmail.com"
 let employPass = "123456789"
 
 // We cannot change the 'const'. 
+// WE will neer use 'var' data type because of issue in block scope and functional scope.
 employPass = "2315990055"
 employCity = "Aligarh"
 
